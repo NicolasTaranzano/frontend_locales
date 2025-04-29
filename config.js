710e1e19f8dfec54193e403ba5d27112a3efc03e
NicolasTaranzano <taranzanon@gmail.com>
2025-04-29 20:13:34 -0300

    Actualizado

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-export const API_URL = 'http://192.168.200.22:3000'; // reemplazá con la IP de tu PC
+export const API_URL = 'http://192.168.200.18:3000'; // reemplazá con la IP de tu PC
